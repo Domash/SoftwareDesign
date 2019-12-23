1a0f6c0b5a41d9028fb7ea0d286c3f095b6e38bb
Денис Домашевич <denisdomasevich@MacBook-Pro-Denis.local>
2019-12-23 12:38:14 +0300

    [master]: start rss reader project

--- RssReader/app/src/main/java/com/domash/rssreader/MainActivity.kt
+++ RssReader/app/src/main/java/com/domash/rssreader/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.domash.rssreader.network.NetworkReceiver
 
-
 class MainActivity : AppCompatActivity() {
 
 
